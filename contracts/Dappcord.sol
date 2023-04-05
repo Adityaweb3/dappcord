3c30bf8251e4ab7ab20b0f51a8b0cd430620d402
Adityaweb3 <ap7596112@gmail.com>
2023-04-05 16:18:48 +0530

    Testing

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol" ;
 
-contract Dappcord {}
+contract Dappcord is ERC721 {
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+
+
+    }
+
+
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,51 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappcord", function () {
+
+  let dappcord
+
+  beforeEach(async() => {
+    const Dappcord = await ethers.getContractFactory("Dappcord"),
+      dappcord = await Dappcord.deploy('Dappcord', 'DC')
+
+  })
+
+  describe("Deployment", function () {
+    it("Sets the name", async() => {
+
+
+      // Deploy the contract
+      
+
+      //fetch and check name
+      let result = await dappcord.name()
+      expect(result).to.equal("Dappcord")
+
+      //Fetch and check  symbol 
+      
+    })
+
+
+    it("Sets the symbol", async() => {
+
+
+      // Deploy the contract
+      const Dappcord = await ethers.getContractFactory("Dappcord"),
+      dappcord = await Dappcord.deploy('Dappcord', 'DC')
+
+      
+
+      //Fetch and check  symbol 
+     let  result = await dappcord.symbol()
+      expect(result).to.equal("DC")
+    })
+
+
+  })
 
 })
